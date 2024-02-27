ab5b883f6d1fa0a45131774580edd4632f72310e
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-02-27 20:17:15 +0500

    Add files via upload

---
+++ Section 16/Conditions/app.js
@@ -0,0 +1,79 @@
+// console.log("Conditional Statements");
+
+// let total = 5; 
+
+// if(total == 5)
+// {
+//     console.log("Correct");
+// }
+
+// console.log("After the Condition"); 
+
+// let random = Math.random();
+
+// if(random < 0.5)
+// {
+//     console.log("Random Number is less than 0.5 !!");
+//     console.log(random);
+// }
+// else
+// {
+//     console.log("Random Number is not less than 0.5");
+// }
+
+/*  if and elseif() statements */
+
+
+//let age = prompt("Enter your age :");
+
+// if(age === 0 || age <= 5)
+// {
+//     console.log("Free")
+// }
+// else if(age <=10)
+// {
+//     console.log("Child $10");
+// }
+// else if(age<=65)
+// {
+//     console.log("Adult $20");
+// }
+// else if(age>65)
+// {
+//     console.log("Senior $10");
+// }
+
+
+
+/* Test exercise 2 */
+
+// const phrase = prompt("Enter  a phrase: ");
+// if(phrase === 'stop')
+// {
+//     console.log("red")
+// }
+// else if(phrase === 'slow')
+// {
+//     console.log("yellow");
+// }
+// else if(phrase === 'go')
+// {
+//     console.log("green");
+// }
+// else
+// {
+//     console.log("purple");
+// }
+
+/* code test 3 */
+// let name = prompt("Enter your name:");
+
+// if (name.length > 5) {
+//     do {
+//         console.log("Your Name is too long!!!");
+//         name = prompt("Re-enter your name:");
+//     } while (name.length > 5);
+// } 
+
+// console.log("Your name is: " + name);
+
---
+++ Section 16/truth_falsy/app.js
@@ -0,0 +1,9 @@
+//let number = prompt("Enter a number : ");
+
+if(0)
+{
+    console.log("Truth");
+}
+else{
+    console.log("False");
+}

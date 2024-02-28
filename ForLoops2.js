d7e6fa54382ae34b02a24e320770f3e3ab7f7e1a
Zakir Matloob <145646219+ZakirCodeArchitect@users.noreply.github.com>
2024-02-28 22:02:18 +0500

    selection 19 completed

---
+++ ForLoops2.js
@@ -0,0 +1,67 @@
+prompt = require("prompt-sync")();
+
+// // Data set # 1
+// let names = ['zakir', 'kashan' , 'hassan', 'luqman', 'ali'];
+
+// // For loop type # 1
+// for(let i=0;i<names.length;i++)
+// {
+//     console.log(names[i]);
+// }
+
+// // For loop type # 2 for Dataset # 1 ( more easier to read )
+// for( let all of names)
+// {
+//     console.log(all);
+// }
+
+// // Data Set 2 
+// let city = [
+//     ['lahore', 'rawalpindi', 'karachi'],
+//     ['gilgit', 'peshawar', 'chilas', 'skardu']
+// ];
+
+// console.log("Name of the Cities : ");
+
+// // Method # 1
+// for(let i=0;i<city.length;i++)
+// {
+//     const row = city[i];
+//     for(let j=0;j<row.length;j++)
+//     {
+//         console.log(row[j]);
+//     }
+// }
+
+// // Method 2 ( more easier to read )
+// for(let row of city) // here row is representing the elements of row1
+// {
+//     // For each row this loop will iterate for the max number of elements in it.
+//     for(all of row)
+//     {
+//         console.log(all);
+//     }
+// }
+
+
+// // displaying the alphabets of any string : 
+// let str = prompt("Enter any string : ");
+
+// for(let alp of str)
+// {
+//     console.log(alp);
+// }
+
+/*
+// coding Exercise from udemy: 
+const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+
+for(let sqr of numbers)
+{
+    console.log(sqr*sqr);
+}
+*/
+
+
---
+++ whileloop.js
@@ -0,0 +1,11 @@
+console.log("While loops");
+
+prompt = require("prompt-sync")();
+
+let number = prompt("Enter any number : ");
+
+while(true)
+{
+    number = prompt("Enter any number :" );
+}
+console.log("You win !!");
